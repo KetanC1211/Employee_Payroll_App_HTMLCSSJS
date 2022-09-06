bb5e1f30137a32a9616ebef9d2ea03a0c948a405
KetanC1211 <ketanchindarkar12@gmail.com>
2022-09-06 20:31:28 +0530

    refactor

--- employee.js
+++ employee.js
@@ -6,3 +6,18 @@ window.addEventListener('DOMContentLoaded', (event) =>{
         output.textContent = salary.value; 
     });
 });
+
+// const formContent = document.querySelector('.formContent'),
+//       form =formContent.querySelectorAll('.form'),
+//       submitInput = form[0].querySelector('input[type ="submit"]');
+
+// function getDataForm(e){
+//     e.preventDefault();
+//     var formData = new FormData(form[0]);
+//     alert(formData.get('name')+' '+formData.get('profile')+' '+formData.get('profile')+formData.get('gender'));
+
+// }
+
+// document.addEventListener('DOMContentLoaded',function(){
+//     submitInput.addEventListener('click',getDataForm,false);
+// },false);
